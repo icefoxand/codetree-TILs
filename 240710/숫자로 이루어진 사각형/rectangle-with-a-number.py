db4893956f7678c54cd1664246574660832d37c6
Jiwon Choi <92070555+icefoxand@users.noreply.github.com>
2024-07-10 15:40:45 +0900

    Title: 숫자로 이루어진 사각형, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240710/숫자로 이루어진 사각형/rectangle-with-a-number.py
+++ 240710/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -1,12 +1,13 @@
 N = int(input()) # 4
 
-for r in range(N): # 0 1 2 3
-    if r >0:
-        print()
-    for c in range(N): # 0 1 2 3
-        val = r*N + c + 1
-        if val <=9:
-            print(val, end=" ")
-        elif val >9:
-            s = val//9 # 9로 나눈 몫을 s로 정의
-            print(val - s*9, end=" ")
+def print_num(n):
+    cnt = 1
+    for _ in range(n):
+        for _ in range(n):
+            print(cnt, end=" ")
+            cnt += 1
+            if cnt == 10:
+                cnt =1 # 초기화 하는 작업을 생각 못했음.
+        print() # 개행을 열을 다 채우고 삽입하면, 굳이 r이 0이냐 0이 아니냐를 판단할 필요가 없어짐
+
+print_num(N)
